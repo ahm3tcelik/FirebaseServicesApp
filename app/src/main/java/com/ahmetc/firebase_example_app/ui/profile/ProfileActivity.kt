8672dc7a8cf285377b943e3abd30071cbb925f60
ahm3tcelik <ahm3tcelik72@gmail.com>
2021-04-07 22:40:15 +0300

    Add Profile activity & get, update, delete operations

--- app/src/main/java/com/ahmetc/firebase_example_app/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/ahmetc/firebase_example_app/ui/profile/ProfileActivity.kt
@@ -1,15 +1,21 @@
 package com.ahmetc.firebase_example_app.ui.profile
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.ahmetc.firebase_example_app.R
 import com.ahmetc.firebase_example_app.data.models.User
 import com.ahmetc.firebase_example_app.databinding.ActivityProfileBinding
 import com.ahmetc.firebase_example_app.ui.auth.AuthActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
 
@@ -17,6 +23,16 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private val viewModel: ProfileViewModel by viewModels()
     private lateinit var binding: ActivityProfileBinding
+    private var avatarUrl: String? = null
+
+    companion object {
+        //image pick code
+        private const val IMAGE_PICK_CODE = 1000
+
+        //Permission code
+        private const val PERMISSION_CODE = 1001
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,23 +44,133 @@ class ProfileActivity : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
         viewModel.loadUser(mAuth)
 
+        viewModel.newName = binding.profileTextName.text
+        viewModel.newSurname = binding.profileTextSurname.text
+        viewModel.newAvatarUrl = viewModel.user.value?.avatar_url
+        viewModel.newPassword = binding.profileTextNewPassword.text
+
+        binding.profileBtnSave.setOnClickListener {
+            val newUser = viewModel.user.value?.copy(
+                name = binding.profileTextName.text.toString(),
+                surname = binding.profileTextSurname.text.toString(),
+                avatar_url = avatarUrl
+            )
+            viewModel.updateUser(newUser)
+        }
+        binding.profileBtnCancel.setOnClickListener {
+            handleUser(viewModel.user.value)
+        }
+
+        binding.profileBtnNewPassword.setOnClickListener { viewModel.updatePassword(mAuth) }
+        binding.profileBtnNewPasswordCancel.setOnClickListener {
+            binding.profileTextNewPassword.setText("")
+        }
+
+        binding.profileBtnDelete.setOnClickListener {
+            val builder = MaterialAlertDialogBuilder(this)
+            builder.setTitle("Dikkat")
+            builder.setMessage("Hesabınızın silinmesini onaylıyor musunuz?")
+
+            builder.setNegativeButton("Hayır") { dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.setPositiveButton("Evet") { dialog, _ ->
+                viewModel.deleteUser(mAuth)
+                dialog.dismiss()
+            }
+            builder.show()
+        }
+
+        binding.profileBtnPickAvatar.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED
+                ) {
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE)
+                } else {
+                    //permission already granted
+                    pickImageFromGallery()
+                }
+            } else {
+                //system OS is < Marshmallow
+                pickImageFromGallery()
+            }
+        }
+
+        // Handle View states
         viewModel.user.observe(this, {
             it?.let { handleUser(it) }
         })
+        viewModel.newPasswordViewState.observe(this, {
+            it?.let { handleNewPassword(it) }
+        })
+        viewModel.deleteAccViewState.observe(this, {
+            it?.let { handleDeleteAcc(it) }
+        })
+        viewModel.userViewState.observe(this, {
+            it?.let { handleUserViewState(it) }
+        })
     }
 
-    private fun handleUser(user: User) {
-        binding.profileTextMail.setText(user.email)
-        binding.profileTextName.setText(user.name)
-        binding.profileTextSurname.setText(user.surname)
-        if (user.avatar_url.isNullOrEmpty()) {
+    private fun handleUser(user: User?) {
+        binding.profileTextMail.setText(user?.email)
+        binding.profileTextName.setText(user?.name)
+        binding.profileTextSurname.setText(user?.surname)
+        if (user?.avatar_url.isNullOrEmpty()) {
             binding.profileAvatar.setImageResource(R.drawable.avatar)
+        } else {
+            Picasso.get()
+                .load(user?.avatar_url)
+                .error(R.drawable.error)
+                .placeholder(R.drawable.loading)
+                .into(binding.profileAvatar)
         }
-        Picasso.get()
-            .load(user.avatar_url)
-            .error(R.drawable.error)
-            .placeholder(R.drawable.loading)
-            .into(binding.profileAvatar)
+    }
+
+    private fun handleDeleteAcc(viewState: ProfileViewState) {
+        if (viewState.isFail) {
+            Toast.makeText(this, viewState.errorMsg, Toast.LENGTH_LONG).show()
+        } else if (viewState.isSuccess) {
+            Toast.makeText(this, "Hesabınız başarıyla silindi!", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun handleNewPassword(viewState: ProfileViewState) {
+        if (viewState.isFail) {
+            Toast.makeText(this, viewState.errorMsg, Toast.LENGTH_LONG).show()
+        } else if (viewState.isSuccess) {
+            binding.profileTextNewPassword.setText("")
+            Toast.makeText(this, "Parolanız başarıyla değiştirildi", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun handleUserViewState(viewState: ProfileViewState) {
+        if (viewState.isFail) {
+            Toast.makeText(this, viewState.errorMsg, Toast.LENGTH_LONG).show()
+        } else if (viewState.isSuccess) {
+            Toast.makeText(this, "Profiliniz başarıyla güncellendi", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            val avatarUri = data?.data
+            if (avatarUri != null) {
+                binding.profileAvatar.setImageURI(avatarUri)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -52,7 +178,7 @@ class ProfileActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.profileToolbarSignOut -> {
             mAuth.signOut()
             startActivity(Intent(this, AuthActivity::class.java))
@@ -61,6 +187,25 @@ class ProfileActivity : AppCompatActivity() {
         }
         else -> {
             super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    pickImageFromGallery()
+                } else {
+                    Toast.makeText(this, "Erişim Engelli", Toast.LENGTH_SHORT).show()
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/ahmetc/firebase_example_app/ui/profile/ProfileViewState.kt
@@ -0,0 +1,7 @@
+package com.ahmetc.firebase_example_app.ui.profile
+
+data class ProfileViewState (
+    val isFail: Boolean = false,
+    val isSuccess: Boolean = false,
+    val errorMsg: String? = null
+)
